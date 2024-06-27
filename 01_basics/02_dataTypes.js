f7f110cd6f3a1a5dcf5af47760a6377302fea5b3
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-06-27 05:12:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all js as nwer version
+
+// alert(3+3) // we ar using node js, not browser
+
+console.log(3+3); //code readability should be high
+
+console.log("Rahul Gupta");
+
+
+let name = "Rahul Gupta";
+let age = 22;
+let isLogedIn = false;
+let state = null;
+let number;
+
+// premetive data types
+
+// Datatypes in js
+// number => 2 to power 53
+// bigint
+// string => String values can be stored in single('') or double quotes("")
+// boolean => true/false
+// null => standalone value
+// undefined => variable is declared but value is not initialized
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
