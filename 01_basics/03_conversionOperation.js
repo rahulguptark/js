dfb231d283bbba191e70cbb5f36d6163e5bee614
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-06-28 16:05:00 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33jkbfjew";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let convertInNumber = Number(score);
+// console.log(typeof convertInNumber);
+// console.log(convertInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// 0 => false
+// 1 => true
+// "" => false
+// "Rahul Gupta" => true
+
+let isLoggedIn = "Rahul Gupta";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof(stringNumber));
