6373538a7b0e97b9afa04be751496d4880a0a855
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-06-29 04:51:41 +0000

    Datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(4 > 2);
+// console.log(4 < 2);
+// console.log(4 >= 2);
+// console.log(4 <= 2);
+// console.log(4 != 2);
+// console.log(4 == 2);
+
+// console.log("2" < 3);
+// console.log("02" > 3);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+// === compares the value as well as the datatype
+
+console.log(2 == 2);
+console.log(2 === 2);
+console.log("2" === 2);
