d1fac674a68c22bd94c8cb96750eb95b55ea52d5
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-07-05 06:08:17 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// syntax of switch statement
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default case");
+        break;
+}
